a5bf83636f0be97717262ffecb10d80763a19b10
thomasj49 <thomasj49@nycstudents.net>
2025-09-19 13:10:11 -0400

    Update Feeder.java

--- Feeder.java
+++ Feeder.java
@@ -1,27 +1,51 @@
-public class Feeder {
+    public class Feeder {
 
-    /**
-     * The amount of food, in grams, currently in the bird feeder; initialized
-     * in the constructor and always greater than or equal to zero
-     */
-    private int currentFood;
+        /**
+         * The amount of food, in grams, currently in the bird feeder; initialized
+         * in the constructor and always greater than or equal to zero
+         */
+        private int currentFood;
 
-    /**
-     * Simulates one day with numBirds birds or possibly a bear at the bird
-     * feeder, as described in part (a) Precondition: numBirds > 0
-     */
-    public void simulateOneDay(int numBirds)
-    {
-    }
+        public int getCurrentFood() {
+            return currentFood;
+        }
 
-    /**
-     * Returns the number of days birds or a bear found food to eat at the
-     * feeder in this simulation, as described in part (b) Preconditions:
-     * numBirds > 0, numDays > 0
-     */
-    public int simulateManyDays(int numBirds, int numDays) 
-    {
-        return 0;
-    }
+        public Feeder(int food) {
+            currentFood = food;
+        }
 
-}
+        /**
+         * Simulates one day with numBirds birds or possibly a bear at the bird
+         * feeder, as described in part (a) Precondition: numBirds > 0
+         */
+        public void simulateOneDay(int numBirds) {
+            if (Math.random() < 0.05) currentFood = 0;
+            else {
+                int eaten = (int) (Math.random() * 41) + 10;
+                eaten *=numBirds;
+                currentFood -= eaten;
+            }
+        }
+
+            /**
+             * Returns the number of days birds or a bear found food to eat at the
+             * feeder in this simulation, as described in part (b) Preconditions:
+             * numBirds > 0, numDays > 0
+             */
+            public int simulateManyDays( int numBirds, int numDays)
+            {
+                int days = 0;
+                while (numDays > 0)
+                {
+                    if (currentFood == 0) return days;
+                    simulateOneDay(numBirds);
+                    numDays --;
+                    days++;
+                }
+                return days;
+            }
+
+
+        }
+
+
