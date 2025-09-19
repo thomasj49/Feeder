827ce7b8afef3e95b89d207394a8903cdaabc224
thomasj49 <thomasj49@nycstudents.net>
2025-09-19 13:09:51 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -2,6 +2,16 @@ public class Main
 {
     public static void main(String[] args)
     {
-        Feeder f = new Feeder();
+
+        Feeder f = new Feeder(500);
+        System.out.println(f);
+        System.out.println("Current Food: " + f.getCurrentFood());
+        f.simulateOneDay(12);
+        System.out.println("Current Food: " + f.getCurrentFood());
+        f = new Feeder(250);
+        f.simulateOneDay(22);
+        System.out.println("Current Food: " + f.getCurrentFood());
+        f.simulateOneDay(0);
+        System.out.println(f.simulateManyDays(10,5) + " days");
     }
 }
